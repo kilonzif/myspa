abaa7a14dedccac9ff6c0cf2ff8968fa7766fd23
FaithK <faith.kilonzi@makeduconsult.com>
2020-05-05 17:34:24 +0000

    an introduction to spas

---
+++ resources/js/app.js
@@ -0,0 +1,24 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import App from './views/App'
+import Welcome from './views/Welcome'
+
+const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Welcome
+        },
+    ],
+});
+
+const app = new Vue({
+    el: '#app',
+    components: { App },
+    router,
+});
---
+++ resources/js/views/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container">
+                <ul>
+                    <router-link :to="{name: 'home'}" class="navbar-brand">Home</router-link>
+                </ul>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto"></ul>
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-link"> App</li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <main class="py-4">
+            <router-view></router-view>
+        </main>
+    </div>
+</template>
+
+
+<script>
+    export default {}
+</script>
